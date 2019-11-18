b22244f6209ae323d2e756071e39b329bf57d3bd
Alex Hoffman <alxhoff@gmail.com>
2019-11-18 17:31:21 +0100

    Started

---
+++ async_UDP/build/CMakeFiles/bar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/bar"
+  "../bin/bar.pdb"
+  "CMakeFiles/bar.dir/src/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
