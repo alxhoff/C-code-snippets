e64e7226da7fe4746e46ad29cb2fd9e0323cec39
Alex Hoffman <alxhoff@gmail.com>
2019-11-21 19:26:55 +0100

    More random code while killing time at work :P

--- embedded_button_checker/src/buttons.c
+++ embedded_button_checker/src/buttons.c
@@ -21,6 +21,7 @@ struct buttons {
 	SemaphoreHandle_t lock;
 
 	button_t head;
+	unsigned int button_count;
 } bt = { 0 };
 
 signed char checkButton(button_t *but)
@@ -35,29 +36,105 @@ signed char checkButton(button_t *but)
 	return -1;
 }
 
-//TODO add/delete button
 button_t *buttonFind(unsigned char identifier)
 {
-	return NULL;
+	button_t *iterator;
+
+	xSemaphoreTake(bt.lock, portMAX_DELAY);
+
+	for (iterator = &bt.head;
+	     iterator->next && (iterator->next->identifier != identifier);
+	     iterator = iterator->next)
+		;
+
+	xSemaphoreGive(bt.lock);
+
+	return iterator->next;
 }
-void buttonAdd(int (*poll)(int, int), int port_arg, int pin_arg)
+void buttonAdd(char identifier, int (*poll)(int, int), int port_arg,
+	       int pin_arg)
 {
-	return;
+	button_t *iterator;
+
+	if (bt.lock)
+		xSemaphoreTake(bt.lock, portMAX_DELAY);
+
+	for (iterator = &bt.head; iterator->next; iterator = iterator->next)
+		;
+
+	iterator->next = calloc(1, sizeof(button_t));
+
+	if (!iterator->next) {
+		xSemaphoreGive(bt.lock);
+		fprintf(stderr, "Allocating button failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	iterator->poll = poll;
+	iterator->port_arg = port_arg;
+	iterator->pin_arg = pin_arg;
+	iterator->identifier = identifier;
+	bt.button_count++;
+	if (bt.lock) // In case called before task has been started and semaphore init'd
+		xSemaphoreGive(bt.lock);
 }
 
-void buttonDelete(unsigned char indentifier)
+void buttonDelete(unsigned char identifier)
 {
-	return;
+	button_t *iterator, *delete;
+
+	if (bt.lock)
+		xSemaphoreTake(bt.lock, portMAX_DELAY);
+
+	for (iterator = &bt.head;
+	     iterator->next && (iterator->next->identifier != identifier);
+	     iterator = iterator->next)
+		;
+
+	if (iterator->next)
+		if (iterator->next->identifier == identifier) {
+			delete = iterator->next;
+			if (!delete->next)
+				iterator->next = NULL;
+			else
+				iterator->next = delete->next;
+			free(delete);
+			bt.button_count--;
+		}
+
+	if (bt.lock)
+		xSemaphoreGive(bt.lock);
 }
 
 //TODO poll buttons
-unsigned char *buttonGetPresses(void)
+char *buttonGetPresses(void)
 {
-	return NULL;
+	char *ret;
+	button_t *iterator;
+
+	xSemaphoreTake(bt.lock, portMAX_DELAY);
+
+	ret = calloc(bt.button_count, sizeof(char));
+	if (!ret) {
+		fprintf(stderr,
+			"Could not allocate return buffer for poll buttons\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (iterator = &bt.head; iterator->next; iterator = iterator->next)
+		strncat(ret,
+			checkButton(iterator->next) ? &iterator->identifier :
+						      "",
+			1);
+
+	xSemaphoreGive(bt.lock);
+	return ret;
 }
 
 void buttonTask(void *parameters)
 {
+	buttonInit();
+
 	while (1) {
 	}
 }
