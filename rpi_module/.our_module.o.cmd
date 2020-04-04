3487dc137cbe0c7735c1bebf921878642051e3d3
Alex Hoffman <alxhoff@gmail.com>
2020-04-04 16:01:20 +0200

    Added gitignore for RPi LKM

--- rpi_module/.our_module.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alxhoff/git/GitHub/C-code-snippets/rpi_module/our_module.o := ~/tools/arm-bcm2708/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-ld -EL   -r -o /home/alxhoff/git/GitHub/C-code-snippets/rpi_module/our_module.o /home/alxhoff/git/GitHub/C-code-snippets/rpi_module/module.o 
--- rpi_module/.tmp_versions/our_module.mod
+++
@@ -1,3 +0,0 @@
-/home/alxhoff/git/GitHub/C-code-snippets/rpi_module/our_module.ko
-/home/alxhoff/git/GitHub/C-code-snippets/rpi_module/module.o
-
